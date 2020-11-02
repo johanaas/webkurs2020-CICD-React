a9242270f26215565242f864ddf25e3a6392c8cf
Johannes Åsheim <joh.aasheim@gmail.com>
2020-11-02 18:35:10 +0100

    More header

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const styles = {
 const Header = () => {
     return (
         <div style={styles}>
-            <header>KART !!? auto? </header>
+            <header>KART !!? auto? ? </header>
         </div>
     )
 }
